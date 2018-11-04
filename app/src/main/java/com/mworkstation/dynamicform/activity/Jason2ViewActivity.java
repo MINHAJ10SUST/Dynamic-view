2c519a6ef331099e98a8016c433207f9f2eec2bd
md minhajul islam <minhajul.sust@gmail.com>
2018-11-04 18:02:22 +0600

    jason2view

---
+++ app/src/main/java/com/mworkstation/dynamicform/activity/Jason2ViewActivity.java
@@ -0,0 +1,14 @@
+package com.mworkstation.dynamicform.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Jason2ViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jason2_view);
+    }
+}
